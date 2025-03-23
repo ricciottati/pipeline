7b0236dd3a70fc928a45056e5b75a0489ad68e1a
rzrdt8 <giulio.ricci@aptiv.com>
2025-03-23 16:16:43 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,21 @@
+
+import org.python.util.PythonInterpreter;
 public class Main {
+<<<<<<< Updated upstream
   public static void main(String[] args) {
     System.out.println("Hello World Now");
   }
 }
+=======
+
+  
+
+  public class PythonInJava {
+      public static void main(String[] args) {
+          PythonInterpreter interpreter = new PythonInterpreter();
+          interpreter.zexec("print('Hello from Python!')");
+      }
+  }
+  
+}
+>>>>>>> Stashed changes
